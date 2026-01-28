76d0147364109e894d1058de28e814394eb8d034
mirzacesir <mirza.cesir@stu.ibu.edu.ba>
2026-01-29 00:31:01 +0100

    back to the beggining

--- backend/rest/dao/config.php
+++ backend/rest/dao/config.php
@@ -40,17 +40,7 @@ class Config
    }
 
    public static function get_env($name, $default){
-       if (isset($_ENV[$name]) && trim($_ENV[$name]) !== "") {
-           return $_ENV[$name];
-       }
-       $val = getenv($name);
-       if ($val !== false && trim($val) !== "") {
-           return $val;
-       }
-       if (isset($_SERVER[$name]) && trim($_SERVER[$name]) !== "") {
-           return $_SERVER[$name];
-       }
-       return $default;
+       return isset($_ENV[$name]) && trim($_ENV[$name]) != "" ? $_ENV[$name] : $default;
    }
 
 }
