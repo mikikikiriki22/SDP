31b5ab2984adb0db011db9336ada6ec1751449d2
mirzacesir <mirza.cesir@stu.ibu.edu.ba>
2026-01-28 21:43:36 +0100

    url change

--- frontend/utils/constants.js
+++ frontend/utils/constants.js
@@ -6,8 +6,8 @@
  * - FRONTEND_BASE_URL: Frontend URL (e.g., "https://example.com/")
  */
 const Constants = {
-    PROJECT_BASE_URL: "http://localhost/projekt/backend/",
-    FRONTEND_BASE_URL: "http://localhost/projekt/frontend/",
+    PROJECT_BASE_URL: "https://sea-lion-app-v3ujv.ondigitalocean.app/backend/",
+    FRONTEND_BASE_URL: "https://sea-lion-app-v3ujv.ondigitalocean.app/frontend/",
     USER_ROLE: "user",
     ADMIN_ROLE: "admin"
 }; 
